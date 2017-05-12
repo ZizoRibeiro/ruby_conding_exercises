e9e95b65102af992cb2f3f730570c0bce2e168ed
Zizo Ribeiro <zizo.ribeiro@gmail.com>
2017-05-12 00:50:19 -0700

    Add Return Odd elements from an array in ruby

---
+++ return_odd_elements_from_an_array_in_ruby.rb
@@ -0,0 +1,22 @@
+require 'rspec'
+
+class Array
+  def odd_selector
+    odds = []
+    self.each do |e|
+      odds << e if e.odd?
+    end
+    odds 
+  end
+end
+
+
+describe 'Odd selector' do 
+  it 'given an array, it returns a collection of the odd elements' do 
+    expect(Array(1..10).odd_selector).to eq([1, 3, 5, 7, 9])
+  end
+end
+
+# arr = Array(1..10)
+# arr.odd_selector
+
